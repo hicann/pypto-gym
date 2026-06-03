e849a43896de52186332443b35429af4f4c39f7d
rsj007 <renshuojian@huawei.com>
2026-06-03 16:15:08 +0800

    修复mhc_sinkhorn的reshape用法
    Co-authored-by: rsj007<renshuojian@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !146 merge master into master
    
    修复mhc_sinkhorn的reshape用法
    
    Created-by: rsj007
    Commit-by: rsj007
    Merged-by: cann-robot
    Description: 
    ## 变更描述 / Description
    
    修复mhc_sinkhorn的reshape用法
    ## 改动类型 / Change Type
    
    - [x] Bug 修复 / Bug Fix
    - [ ] 新功能 / New Feature
    - [ ] 性能优化 / Performance
    - [ ] 代码重构 / Refactoring
    - [ ] 文档更新 / Documentation
    - [ ] 测试相关 / Test
    - [ ] 其它 / Other
    
    ## 关联 Issue / Related Issues
    
    <!-- Closes #000 可自动关闭 / Closes #000 to auto-close -->
    
    - Closes #
    - References #
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [x] 单元测试通过 / UT passed
    - [ ] 集成测试通过 / ST passed
    - [ ] 人工验证通过 / Manual verified
    
    ## 检查清单 / Checklist
    
    - [x] 代码符合规范 / Code follows style guide
    - [ ] 测试添加并通过 / Tests added and passed
    - [ ] 文档已更新 / Docs updated if needed
    - [ ] 无硬编码敏感信息 / No secrets hardcoded
    - [ ] 提交信息符合规范 / Commit message follows convention
    
    See merge request: cann/pypto-gym!146

--- src/pypto_gym/ops/pypto_tile/experimental/vector/mhc_sinkhorn/mhc_sinkhorn_impl.py
+++ src/pypto_gym/ops/pypto_tile/experimental/vector/mhc_sinkhorn/mhc_sinkhorn_impl.py
@@ -62,6 +62,7 @@ def mhc_sinkhorn_kernel(
 
         row_sum = pypto.sum(comb_flag, 1, True)
         comb_flag = comb_flag / (row_sum + eps)
+        comb_flag = comb_flag + eps
 
         col_sum = pypto.sum(comb_flag, 0, True)
         comb_flag = comb_flag / (col_sum + eps)
@@ -72,12 +73,12 @@ def mhc_sinkhorn_kernel(
             col_sum = comb_flag.sum( 0, keepdim=True)
             comb_flag = comb_flag / (col_sum + eps)
         
-        comb_flag = pypto.reshape(comb_flag, [hc*hc, tile_t], valid_shape=[hc*hc, t_valid], inplace=True)
+        comb_flag = pypto.reshape(comb_flag, [hc*hc, tile_t])
         pypto.set_vec_tile_shapes(16, 256)
         comb_flag = pypto.transpose(comb_flag, 1, 0)
         pypto.set_vec_tile_shapes(256, 16)
         
-        comb_flag = pypto.reshape(comb_flag, [tile_t, hc, hc], valid_shape=[t_valid, hc, hc], inplace=True)
+        comb_flag = pypto.reshape(comb_flag, [tile_t, hc, hc])
         out[t_idx:, :, :] = comb_flag
 
 
