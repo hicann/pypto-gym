a22416c8d46703fa1c59b94d9846c07355e712a6
blue_jan <zhangjiarui6@huawei.com>
2026-08-17 16:30:32 +0800

    fix(Operator): fa_mha accuracy eroor
    Co-authored-by: blue_jan<zhangjiarui6@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !495 merge master into master
    
    fix(Operator): fa_mha accuracy eroor
    
    Created-by: blue_jan
    Commit-by: blue_jan
    Merged-by: cann-robot
    Description: 
    <!-- 感谢您的合入申请! -->
    
    ### 当前PR是否有AI参与:   
    [x]  否     
    
    [ ]  是   
    __1.  AI Agent 平台:    
    __2.  AI 模型:  
    __3.  Prompt上下文 : 
    
    
    ### PR功能描述 / 为什么需要这个合入**:  
    Use new ir, fix accuracy error,
           
        
    ### 该PR关联的issue   
    *(格式为fixes #<issue号>, 或者resolves #<issue号>)*: fixes [#27](https://gitcode.com/cann/pypto-gym/issues/27) 
    
    ### 希望检视人员了解:
    
    
    ## 改动类型 / Change Type
    
    - [x] Bug 修复 / Bug Fix
    
    
    ## 测试信息 / Testing
    
    <!-- 简要测试说明或关键结果 / Brief test description or key results -->
    
    - [x] 人工验证通过 / Manual verified
    
    
    See merge request: cann/pypto-gym!495

--- src/pypto_gym/ops/pypto_tensor/experimental/ops_transformer/flash_attention_mha/flash_attention_mha_impl.py
+++ src/pypto_gym/ops/pypto_tensor/experimental/ops_transformer/flash_attention_mha/flash_attention_mha_impl.py
@@ -322,7 +322,6 @@ def flash_attention_varlen_forward_950(
 
 
 @pypto.frontend.jit(
-    new_ir=False,
     runtime_options={
         "device_sched_mode": 0,
         "stitch_function_max_num": 1024,
